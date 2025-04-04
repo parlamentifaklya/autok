5a6fe083799374d6a1c9a3c43bcaa4a277b779f8
Kökényesi Andor <20.kokenyesi.andor@mechwart.com>
2025-04-04 10:24:40 +0200

    asd

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -8,9 +8,12 @@ const Cars = () => {
     useEffect(() => {
         const fetchAuto = async () => {
             try {
-                const res = await fetch("autok.json");
-                const data = await res.json();
-                setCars(data);
+                await fetch("autok.json").then(res => res.json()).then(data => {
+                    let apiCars = data.autok;
+                    apiCars = apiCars.sort(() => Math.random() - 0.5);
+                    apiCars = apiCars.slice(0, 4);
+                    setCars(apiCars);
+                });
             } catch (error) {
                 console.error(error);
             }
@@ -19,11 +22,14 @@ const Cars = () => {
         fetchAuto();
     }, [])
 
-
     return (
         <div>
-            {cars.map((car) => (
-                <div key={car.modell}>{car.modell}</div>
+            {cars.map((car, idx) => (
+                <div>
+                    <img src={`https://picsum.photos/id/${Math.floor(Math.random())}/600/400`} style={{ width: 600, height: 400}}/>
+                    <div style={{color: car.sebessegvalto === "automata" ? "blue" : "red"}} key={idx}>{car.marka} {car.modell} - {car.sebessegvalto}</div>
+
+                </div>
             ))}
         </div>
     )
